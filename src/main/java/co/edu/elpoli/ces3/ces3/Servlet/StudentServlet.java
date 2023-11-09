869bca48276ae224960609887bce8098e4ddcb93
Luis Alejandro Calvo Alvarez <offztinger@gmail.com>
2023-11-08 23:15:54 -0500

    Metodos doPUT y doDELETE

--- src/main/java/co/edu/elpoli/ces3/ces3/Servlet/StudentServlet.java
+++ src/main/java/co/edu/elpoli/ces3/ces3/Servlet/StudentServlet.java
@@ -88,13 +88,71 @@ public class StudentServlet extends MyServlet {
         out.flush();
 
     }
-
     public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        JsonObject body = this.getParamsFromPost(request);
 
+        int studentId = body.get("id").getAsInt();
+
+        Student studentToUpdate = null;
+        for (Student student : students) {
+            if (student.getId() == studentId) {
+                studentToUpdate = student;
+                break;
+            }
+        }
+
+        if (studentToUpdate != null) {
+            // Actualiza los campos del estudiante
+            studentToUpdate.setNombre(body.get("nombre").getAsString());
+            studentToUpdate.setCedula(body.get("cedula").getAsString());
+
+            PrintWriter out = response.getWriter();
+            out.println(gson.toJson(studentToUpdate));
+            out.println("\n PUT SUCCESSFUL");
+            out.flush();
+            System.out.println("El estudiante con el id #" + studentId + " ha sido actualizado");
+
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            PrintWriter out = response.getWriter();
+            out.println("\n No se ha encontrado un estudiante con el id #"+studentId);
+            out.flush();
+        }
     }
 
     public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
 
+        String studentId = request.getParameter("studentId");
+        if (studentId != null) {
+            int idToDelete = Integer.parseInt(studentId);
+            Student studentToDelete = null;
+
+            for (Student student : students) {
+                if (student.getId() == idToDelete) {
+                    studentToDelete = student;
+                    break;
+                }
+            }
+
+            if (studentToDelete != null) {
+                students.remove(studentToDelete);
+                PrintWriter out = response.getWriter();
+                out.println("\n El estudiante con el id #" + idToDelete + " ha sido eliminado.");
+                out.flush();
+            } else {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                PrintWriter out = response.getWriter();
+                out.println("No se ha encontrado un estudiante con el id #"+idToDelete);
+                out.flush();
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            PrintWriter out = response.getWriter();
+            out.println("\n Debe ingresar un id de estudiante por parametros.");
+            out.flush();
+        }
+        System.out.println("El estudiante con el id #" + studentId + " ha sido eliminado");
     }
-
 }
